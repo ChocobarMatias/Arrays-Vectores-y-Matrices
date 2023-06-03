5a4834ba3c2b99b5db6b155fd807933c1eea6513
ChocobarMatias <111932821+ChocobarMatias@users.noreply.github.com>
2023-06-03 01:43:39 -0300

    Agregar archivos de proyecto.

---
+++ Trabajo Practico N 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_Practico_N_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
